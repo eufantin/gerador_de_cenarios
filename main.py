e24015b211aee31f9061641c693ca82876c2ea79
Victor Fantin <vicctorfantin@gmail.com>
2026-02-03 10:42:18 -0300

    estrutura inicial
    estrutura inicial do projeto com as listas

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+missoes = [
+    "Infiltração",
+    "Resgate",
+    "Reconhecimento",
+    "Sabotagem",
+    "Extração"
+]
+
+ambientes = [
+    "Data center abandonado",
+    "Base militar secreta",
+    "Cidade Cyberpunk",
+    "Complexo Industrial"
+]
+
+riscos = [
+    "Baixo",
+    "Médio",
+    "Alto",
+    "Extremo"
+]
+
+estilos = [
+    "RPG"
+    "Cyberpunk",
+    "Militar"
+]
+
